7609ebe75c60b01025894d747a32286a498e86b8
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-31 16:59:38 +0000

    Create SecPi email sender module

---
+++ src/mail.py
@@ -0,0 +1,27 @@
+import settings
+
+from email.mime.text import MIMEText
+from email.mime.image import MIMEImage
+from email.mime.multipart import MIMEMultipart
+import smtplib
+
+
+# FUNCTION TO SEND A SECPI EMAIL #
+def send_secpi_email(image, img_name: str) -> None:
+	msg = MIMEMultipart()
+	msg['Subject'] = settings.EMAILSUBJECT
+	msg['From'] = settings.EMAILSENDER
+	msg['To'] = settings.EMAILRECEIVER
+
+	text = MIMEText(settings.EMAILTEXT)
+	msg.attach(text)
+	image = MIMEImage(image, name=img_name)
+	msg.attach(image)
+
+	s = smtplib.SMTP(settings.EMAILSERVERHOST, settings.EMAILSERVERPORT)
+	s.ehlo()
+	s.starttls()
+	s.ehlo()
+	s.login(settings.EMAILSENDERUSR, settings.EMAILSENDERPSW)
+	s.sendmail(From, To, msg.as_string())
+	s.quit()
