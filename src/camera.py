df2f412d63f96fb79991490ddf8fd0bfee837670
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2020-12-31 17:33:04 +0000

    Create camera module with get_actual_camera_frame function

---
+++ src/camera.py
@@ -0,0 +1,14 @@
+import settings
+import tools
+import picamera
+
+# FUNCTION TO GET THE ACTUAL CAMERA FRAME #
+def get_actual_camera_frame() -> file:
+	camera_output = open('.secpi.camera.tmp')
+	with picamera.PiCamera() as camera:
+		try:
+			camera.capture(camera_output, 
+			               settings.IMAGEFORMAT,
+				       use_video_port=settings.USEVIDEOPORT)
+		except Exception as e:
+			tools.error(e)
