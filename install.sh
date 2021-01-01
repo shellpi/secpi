bff3a2a80b7be75e934dcdb8291dbe9ed86c335e
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-01 01:22:49 +0000

    Create instalation script (install.sh)

---
+++ install.sh
@@ -0,0 +1,15 @@
+echo "Getting SecPi source..."
+curl https://github.com/shellpi/secpi/archive/main.zip -o secpi.zip > /dev/null
+unzip secpi.zip > /dev/null
+
+echo "Installing dependencies..."
+pip install -r secpi/requirements.txt > /dev/null
+
+echo "Locating SecPi..."
+mkdir /usr/secpi
+cp secpi/bin/secpi /usr/secpi/
+cp -r src /usr/secpi/
+export PATH=$PATH:/usr/secpi
+
+
+echo "SecPi installation complete"
