9a06b1ac7fda8944f5629f6957faa5b7a2e7befe
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-02 20:57:12 +0000

    Update main file (secpi.py)

--- src/secpi.py
+++ src/secpi.py
@@ -1,1 +1,16 @@
-print ("Hello, World")
+from mail import send_secpi_email
+from camera import get_actual_camera_frame
+from detect import detect_if_people
+
+from time import sleep
+
+# MAIN FUNCTION #
+def main() -> None:
+	while True:
+		frame = get_actual_camera_frame()
+		if detect_if_people(frame):
+			send_secpi_email(frame, 'Camera view')
+			sleep(10000) #Wait 10 seconds
+		else:
+			pass
+	return None
