d9cc9982ad8624d308749ce91ae58ebd7003749a
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-02 21:00:43 +0000

    SecPi 0.1.0 release

--- src/secpi.py
+++ src/secpi.py
@@ -6,9 +6,11 @@ from time import sleep
 
 # MAIN FUNCTION #
 def main() -> None:
+	print ('SecPi 0.1.0 by ShellPi\n')
 	while True:
 		frame = get_actual_camera_frame()
 		if detect_if_people(frame):
+			print ('PERSON DETECTED!!')
 			send_secpi_email(frame, 'Camera view')
 			sleep(10000) #Wait 10 seconds
 		else:
