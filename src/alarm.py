4363ea3a1a9536496b8b7296a4e325ae1d647bbf
SOMEONE360 <61124185+SOMEONE360@users.noreply.github.com>
2021-01-03 14:08:31 +0000

    Play alarm sound & turn led ON while button is not pressed (alarm.py, alarm function)

--- src/alarm.py
+++ src/alarm.py
@@ -1,6 +1,34 @@
 import settings
+import tools
 from playsound import playsound
+from gpiozero import LED, Button
+
+from time import sleep
+import thread
+
 
 # FUNCTION TO PLAY ALARM SOUND #
-def play_alarm_sound() -> None:
-	playsound(settings.ALARMSOUND)
+def play_alarm_sound(button: Button) -> None:
+	while !button.is_pressed:
+		playsound(settings.ALARMSOUND)
+
+
+# FUNCTION TO TURN THE ALARM LED ON #
+def alarm_led_ON(led: LED, button: Button) -> None:
+	while !button.is_pressed:
+		led.on()
+		sleep(1000)
+		led.off
+		sleep(1000)
+
+
+# PLAY THE ALARM WHILE BUTTON IS NOT PRESSED #
+def alarm() -> None:
+	button = Button(settings.BUTTONPIN)
+	led    = LED(settings.LEDPIN)
+
+	try:
+		thread.start_new_thread( play_alarm_sound, (button) )
+		thread.start_new_thread( alarm_led_ON, (led, button) )
+	except Exception as e:
+		tools.error(e, 'MultithreadingError')
